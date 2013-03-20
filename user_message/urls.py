fabf710ceac88c5e60355abefc868edc2005ce4f
softside <thinkba.zl@gmail.com>
2013-03-20 18:32:12 +0800

    ..

--- user_message/urls.py
+++ user_message/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from message import views as messages_views
+from django.contrib.auth.decorators import login_required
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$',login_required(messages_views.MessageListView.as_view())),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^message/', include('message.urls')),
 )
